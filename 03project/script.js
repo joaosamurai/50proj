91bb9920dfeabe540aac6c0746402550bfd67c9b
João Samurai <joaogufsc@gmail.com>
2022-09-01 18:55:16 -0300

    ex4

--- 03project/script.js
+++ 03project/script.js
@@ -2,6 +2,5 @@ const open = document.getElementById('open')
 const close = document.getElementById('close')
 const container = document.querySelector('.container')
 open.addEventListener('click',()=> {
-    console.log('ok')
     container.classList.add('show-nav')})
 close.addEventListener('click',() => container.classList.remove('show-nav'))
