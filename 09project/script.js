36b1ee66811bf2ffe8205043125ac618071fb629
João Samurai <joaogufsc@gmail.com>
2022-09-02 03:31:43 -0300

    ex 7,8 e 9

---
+++ 09project/script.js
@@ -0,0 +1,18 @@
+const sounds = ['Ratinhooo','victory','yes']
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+    btn.innerText = sound
+    btn.addEventListener('click',() => {
+        stopSongs()
+        document.getElementById(sound).play()
+    })
+    document.getElementById('buttons').appendChild(btn)
+})
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+        song.pause()
+        song.currentTime = 0;
+    })
+}
